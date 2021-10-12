2423bba156b042d84e2f3fc45c9fa7f7d306f579
Sean Craig <seancraig21@gmail.com>
2021-10-12 13:01:51 -0500

    got data from the non profit api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-console.log("js is linked");
+var debugDiv = document.getElementById("debug");
+
+function getNonProfits(str) {
+  var nonProfitURL =
+    "https://projects.propublica.org/nonprofits/api/v2/search.json?q=" + str;
+
+  var corsWorkAroundURL = "https://corsanywhere.herokuapp.com/" + nonProfitURL;
+
+  fetch(corsWorkAroundURL, {
+    headers: {
+      "accept": "application/json",
+      "x-requested-with": "xmlhttprequest",
+      "Access-Control-Allow-Origin": "*",
+      // "Authorization": `Bearer ${apiKey}`
+      // 'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayNonProfits(data);
+      });
+    }
+  });
+}
+
+function getRents(str) {}
+
+function displayNonProfits(data) {
+  console.log(data);
+  
+  var orgNameEl = document.createElement("h2");
+  var orgCityEl = document.createElement("h3");
+  var orgStateEl = document.createElement("h3");
+
+  orgNameEl.textContent = data.organizations[0].name;
+  orgCityEl.textContent = data.organizations[0].city;
+  orgStateEl.textContent = data.organizations[0].state;
+
+  debugDiv.appendChild(orgNameEl);
+  debugDiv.appendChild(orgCityEl);
+  debugDiv.appendChild(orgStateEl);
+}
+
+getNonProfits("chicago");
