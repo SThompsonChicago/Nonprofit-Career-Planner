bef3c61b2e5d4cf076c7b6c84ac5cb188c528f4c
Stephen Thompson <s31@umbc.edu>
2021-10-13 10:52:04 -0500

    display list of old searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("js is linked");
+var cityInput = document.querySelector("#city-text");
+var cityForm = document.querySelector("#city-form");
+var cityList = document.querySelector("#city-list");
+var cityCountSpan = document.querySelector("#city-count");
+
+var cities = [];
+
+function renderCityList(){
+
+    cityList.innerHTML = "";
+    cityCountSpan.textContent = city.length;
+
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+    
+        var li = document.createElement("li");
+        li.textContent = city;
+        li.setAttribute("data-index", i);
+        cityList.appendChild(li);
+    }
+
+}
+
+
