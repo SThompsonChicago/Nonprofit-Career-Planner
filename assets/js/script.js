15ad5c11bf4709c90361d81cbd7a353777cecccc
Sean Craig <seancraig21@gmail.com>
2021-10-14 13:07:19 -0500

    got rent api working and added some spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ var $nonProfitsEl = document.getElementById("nonprofits");
 var $cityFormEl = document.getElementById("city-form");
 var $citySearch = document.getElementById("city-search");
 var $searchHistoryEl = document.getElementById("search-history");
+var $rentDiv = document.getElementById("rent");
 var $modalEl = document.getElementById("myModal");
 var $modalText = document.getElementById("modal-text");
 var $closeSpan = document.getElementsByClassName("close")[0];
+
+const openAPIKey = "17df62e6c611e766e40ad0ad3ee5ec14";
+const rapidAPIKey = "b13b2a8716msha990e8ed4308bffp1b1aebjsn790614efd52b";
 
 var cities = [];
 
@@ -27,6 +31,7 @@ function formSubmitHandler(event) {
 
     localStorage.setItem("city-history", JSON.stringify(cities));
     renderCityList();
+    getLatAndLon(cityName);
     getNonProfits(cityName);
   }
 }
@@ -36,6 +41,7 @@ function buttonSearchHandler(event) {
   var btn = event.target;
   var city = btn.getAttribute("data-search");
 
+  getLatAndLon(city);
   getNonProfits(city);
 }
 
@@ -56,6 +62,24 @@ function renderCityList() {
 
     $searchHistoryEl.appendChild(button);
   }
+}
+
+function getLatAndLon(cityName) {
+  var openWeatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    openAPIKey;
+
+  fetch(openWeatherURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        getRentInfo(cityName, lat, lon);
+      });
+    }
+  });
 }
 
 function openModal(errorText) {
@@ -88,16 +112,41 @@ function getNonProfits(str) {
   }).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        displayNonProfits(data);
+        displayNonProfits(str, data);
       });
     }
   });
 }
 
-function getRent(str) {}
+function getRentInfo(city, lat, lon) {
+  var rentAPIURL =
+    "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?&latitude=" +
+    lat +
+    "&longitude=" +
+    lon;
 
-function displayNonProfits(data) {
+  fetch(rentAPIURL, {
+    headers: {
+      "X-RapidAPI-Key": rapidAPIKey,
+    },
+  }).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayRentInfo(city, data);
+      });
+    }
+  });
+}
+
+function displayNonProfits(str, data) {
   $nonProfitsEl.innerHTML = "";
+
+  var $nonProfitsLabel = document.createElement("label");
+  $nonProfitsLabel.textContent = "Non-Profits in " + str;
+  $nonProfitsLabel.classList.add("label");
+  $nonProfitsEl.appendChild($nonProfitsLabel);
+
   for (var i = 0; i < data.organizations.length; i++) {
     var $resultsDiv = document.createElement("div");
     var $orgNameEl = document.createElement("h2");
@@ -114,6 +163,27 @@ function displayNonProfits(data) {
     $resultsDiv.appendChild($orgNameEl);
     $resultsDiv.appendChild($orgCityAndStateEl);
   }
+}
+
+function displayRentInfo(city, data) {
+  $rentDiv.innerHTML = "";
+
+  var $rentLabel = document.createElement("label");
+  var $aveRent = document.createElement("p");
+  var $highRent = document.createElement("p");
+  var $lowRent = document.createElement("p");
+
+  $rentLabel.textContent = "Rent costs in " + city;
+  $rentLabel.classList.add("label");
+  $rentDiv.appendChild($rentLabel);
+
+  $aveRent.textContent = "$" + data.rent;
+  $highRent.textContent = "$" + data.rentRangeHigh;
+  $lowRent.textContent = "$" + data.rentRangeLow;
+
+  $rentDiv.appendChild($aveRent);
+  $rentDiv.appendChild($lowRent);
+  $rentDiv.appendChild($highRent);
 }
 
 // Helper function to capitalize the first letter of each word
