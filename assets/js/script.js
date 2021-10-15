dfb497a763bce73229083e2b98aaa7425fe13726
Sean Craig <seancraig21@gmail.com>
2021-10-14 21:30:12 -0500

    bedrooms, google and filtering nonprofilt features

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var $cityFormEl = document.getElementById("city-form");
 var $citySearch = document.getElementById("city-search");
 var $searchHistoryEl = document.getElementById("search-history");
 var $rentDiv = document.getElementById("rent");
+var $bedroomSelectForm = document.getElementById("bedrooms-select");
+var $bedroomSelection = document.getElementById("bedrooms");
 var $modalEl = document.getElementById("myModal");
 var $modalText = document.getElementById("modal-text");
 var $closeSpan = document.getElementsByClassName("close")[0];
@@ -10,9 +12,10 @@ var $closeSpan = document.getElementsByClassName("close")[0];
 const openAPIKey = "17df62e6c611e766e40ad0ad3ee5ec14";
 const rapidAPIKey = "b13b2a8716msha990e8ed4308bffp1b1aebjsn790614efd52b";
 
+var currentCity = "";
 var cities = [];
 
-function formSubmitHandler(event) {
+function searchFormSubmitHandler(event) {
   event.preventDefault();
 
   var cityName = $citySearch.value.trim();
@@ -30,10 +33,19 @@ function formSubmitHandler(event) {
     }
 
     localStorage.setItem("city-history", JSON.stringify(cities));
+    currentCity = cityName;
     renderCityList();
-    getLatAndLon(cityName);
+    getLatAndLon(cityName, 0);
     getNonProfits(cityName);
   }
+}
+
+function bedroomFormSubmitHandler(event) {
+  event.preventDefault();
+  var numOfBedrooms = $bedroomSelection.value;
+  console.log(numOfBedrooms);
+  console.log(currentCity);
+  getLatAndLon(currentCity, numOfBedrooms);
 }
 
 function buttonSearchHandler(event) {
@@ -41,12 +53,18 @@ function buttonSearchHandler(event) {
   var btn = event.target;
   var city = btn.getAttribute("data-search");
 
-  getLatAndLon(city);
+  currentCity = city;
+  getLatAndLon(city, 0);
   getNonProfits(city);
 }
 
 function renderCityList() {
   $searchHistoryEl.innerHTML = "";
+
+  var $pastSearchesLabel = document.createElement("label");
+  $pastSearchesLabel.textContent = "Past Searches";
+  $pastSearchesLabel.classList.add("label");
+  $searchHistoryEl.appendChild($pastSearchesLabel);
 
   for (var i = 0; i < cities.length; i++) {
     var city = cities[i];
@@ -64,7 +82,7 @@ function renderCityList() {
   }
 }
 
-function getLatAndLon(cityName) {
+function getLatAndLon(cityName, numOfBedrooms) {
   var openWeatherURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
@@ -76,26 +94,11 @@ function getLatAndLon(cityName) {
       response.json().then(function (data) {
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        getRentInfo(cityName, lat, lon);
+        getRentInfo(cityName, lat, lon, numOfBedrooms);
       });
     }
   });
 }
-
-function openModal(errorText) {
-  $modalText.textContent = errorText;
-  $modalEl.style.display = "block";
-}
-
-$closeSpan.onclick = function () {
-  $modalEl.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == $modalEl) {
-    $modalEl.style.display = "none";
-  }
-};
 
 function getNonProfits(str) {
   var nonProfitURL =
@@ -118,12 +121,14 @@ function getNonProfits(str) {
   });
 }
 
-function getRentInfo(city, lat, lon) {
+function getRentInfo(city, lat, lon, numOfBedrooms) {
   var rentAPIURL =
-    "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?&latitude=" +
-    lat +
-    "&longitude=" +
-    lon;
+    "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?";
+  if (numOfBedrooms > 0) {
+    rentAPIURL = rentAPIURL + "bedrooms=" + numOfBedrooms;
+  }
+
+  rentAPIURL = rentAPIURL + "&latitude=" + lat + "&longitude=" + lon;
 
   fetch(rentAPIURL, {
     headers: {
@@ -132,7 +137,6 @@ function getRentInfo(city, lat, lon) {
   }).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
         displayRentInfo(city, data);
       });
     }
@@ -146,28 +150,37 @@ function displayNonProfits(str, data) {
   $nonProfitsLabel.textContent = "Non-Profits in " + str;
   $nonProfitsLabel.classList.add("label");
   $nonProfitsEl.appendChild($nonProfitsLabel);
-
+  console.log(str.toUpperCase());
   for (var i = 0; i < data.organizations.length; i++) {
-    var $resultsDiv = document.createElement("div");
-    var $orgNameEl = document.createElement("h2");
-    var $orgCityAndStateEl = document.createElement("h3");
-
-    $orgNameEl.textContent = data.organizations[i].name;
-    $orgCityAndStateEl.textContent =
-      data.organizations[i].city + ", " + data.organizations[i].state;
-
-    $resultsDiv.classList.add("box");
-    $resultsDiv.classList.add("content");
-
-    $nonProfitsEl.appendChild($resultsDiv);
-    $resultsDiv.appendChild($orgNameEl);
-    $resultsDiv.appendChild($orgCityAndStateEl);
+    if (data.organizations[i].city === str.toUpperCase()) {
+      var $resultsDiv = document.createElement("div");
+      var $orgNameEl = document.createElement("a");
+      var $orgCityAndStateEl = document.createElement("h3");
+  
+      $orgNameEl.textContent = data.organizations[i].name;
+      $orgNameEl.setAttribute(
+        "href",
+        "https://www.google.com/search?q=" + data.organizations[i].name
+      );
+      $orgNameEl.setAttribute("target", "_blank");
+      $orgCityAndStateEl.textContent =
+        data.organizations[i].city + ", " + data.organizations[i].state;
+  
+      $resultsDiv.classList.add("box");
+      $resultsDiv.classList.add("content");
+  
+      $nonProfitsEl.appendChild($resultsDiv);
+      $resultsDiv.appendChild($orgNameEl);
+      $resultsDiv.appendChild($orgCityAndStateEl);
+    }
+    
   }
 }
 
 function displayRentInfo(city, data) {
   $rentDiv.innerHTML = "";
 
+  // var $rentResultDiv = document.createElement("div");
   var $rentLabel = document.createElement("label");
   var $aveRent = document.createElement("p");
   var $highRent = document.createElement("p");
@@ -176,15 +189,38 @@ function displayRentInfo(city, data) {
   $rentLabel.textContent = "Rent costs in " + city;
   $rentLabel.classList.add("label");
   $rentDiv.appendChild($rentLabel);
+  // $rentDiv.appendChild($rentResultDiv);
+  // $rentResultDiv.appendChild($rentLabel);
 
-  $aveRent.textContent = "$" + data.rent;
-  $highRent.textContent = "$" + data.rentRangeHigh;
-  $lowRent.textContent = "$" + data.rentRangeLow;
+  $aveRent.textContent = "Average Rent: $" + data.rent;
+  $highRent.textContent = "High End Rent: $" + data.rentRangeHigh;
+  $lowRent.textContent = "Low End Rent: $" + data.rentRangeLow;
 
   $rentDiv.appendChild($aveRent);
   $rentDiv.appendChild($lowRent);
   $rentDiv.appendChild($highRent);
+  // $rentResultDiv.appendChild($aveRent);
+  // $rentResultDiv.appendChild($lowRent);
+  // $rentResultDiv.appendChild($highRent);
+
+  $bedroomSelectForm.style.display = "block";
 }
+
+// Modal stuff
+function openModal(errorText) {
+  $modalText.textContent = errorText;
+  $modalEl.style.display = "block";
+}
+
+$closeSpan.onclick = function () {
+  $modalEl.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == $modalEl) {
+    $modalEl.style.display = "none";
+  }
+};
 
 // Helper function to capitalize the first letter of each word
 function capFirstLetter(str) {
@@ -196,6 +232,7 @@ function capFirstLetter(str) {
   return words.join(" ");
 }
 
+// Gets stored cities from local storage
 function init() {
   var storedHistory = JSON.parse(localStorage.getItem("city-history"));
   if (storedHistory !== null) {
@@ -205,5 +242,7 @@ function init() {
 }
 
 init();
-$cityFormEl.addEventListener("submit", formSubmitHandler);
+
+$cityFormEl.addEventListener("submit", searchFormSubmitHandler);
 $searchHistoryEl.addEventListener("click", buttonSearchHandler);
+$bedroomSelectForm.addEventListener("submit", bedroomFormSubmitHandler);
